a6583f2c3f77bebf05a3422b3ad8ba4310b01727
Bhavesh Kumar <bhawishdhanjanii@gmail.com>
2023-03-27 20:57:20 +0500

    Differenrt Ways Of Creating Streams

---
+++ CreatingStreamsDemo.java
@@ -0,0 +1,26 @@
+package Streams;
+
+import Generics.List;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.stream.Stream;
+
+public class CreatingStreamsDemo {
+    public static void show(){
+        var list = new ArrayList<Integer>();
+        list.stream();
+//        Array Stream
+        int[] numbers = {1 ,2 ,3};
+        Arrays.stream(numbers).forEach(n-> System.out.println(n));
+//        Stream of arbitrary number of object
+        var stream1 = Stream.of(1,2,3);
+        stream1.forEach(n-> System.out.println(n));
+//        Generating Infinite Stream
+        var stream2 = Stream.generate(()->Math.random());
+        stream2.limit(10).forEach(n-> System.out.println(n));
+//        Using iterate method creating stream
+        var stream3 = Stream.iterate(1,(n -> n+1));
+        stream3.limit(10).forEach(n-> System.out.println(n));
+    }
+}
